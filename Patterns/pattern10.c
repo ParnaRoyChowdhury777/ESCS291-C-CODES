abe1da6617feaa67d86abaa4d4d1db916a579fc8
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-06-14 11:38:02 +0530

    pattern

---
+++ Patterns/pattern10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  int size;
+  printf("Enter the number of rows in the upper triangle of the hour glass: \n");
+  scanf("%d",&size);
+
+  // reversed pyramid star pattern
+  for (int i = 0; i < size; i++) {
+    // printing spaces
+    for (int j = 0; j < i; j++) {
+      printf(" ");
+    }
+
+    // printing star
+    for (int k = 0; k < (size-i)*2-1; k++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  // pyramid star pattern
+  for (int i = 2; i <= size; i++) {
+    // printing spaces
+    for (int j = size; j > i; j--) {
+      printf(" ");
+    }
+    
+    // printing star
+    for (int k = 0; k < i*2-1; k++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
