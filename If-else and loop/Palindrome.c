0dd9132840a4c97a181723abcf13d5a4610f8bda
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-06-14 10:43:21 +0530

    palindrome

--- If-else and loop/Palindrome.c
+++ If-else and loop/Palindrome.c
@@ -1,20 +1,41 @@
 #include<stdio.h>
+#include<math.h>
+
+long reverse(long x){
+    long temp=x;
+    int d;
+    long rev1=0;
+    if(x<0)
+    x=x*(-1);
+    while(x>0)
+    {
+        d=x%10;
+        x=x/10;
+        rev1=(rev1*10)+d;
+    }
+    if(temp<0)
+    rev1=rev1*(-1);
+    if(rev1<-pow(2,31) || rev1>(pow(2,31)-1))
+    return 0;
+    return rev1;
+}
+
 int main()
 {
-int num,d,copy,rev=0;
+long num,rev;
 printf("\n Enter the number : ");
 scanf("%d",&num);
-copy=num;
-while(num>0)
-{
-d=num%10;
-num=num/10;
-rev=(rev*10)+d;
-}
-if(copy==rev)
-printf("\n %d is a palindrome number",copy);
+
+rev=reverse(num);
+
+if(rev==0)
+printf("Reversing the number causes the value to go outside the signed 32-bit integer range [-231, 231 - 1]");
+
+if(num==rev)
+printf("\n %d is a palindrome number",num);
 else
-printf("\n %d is not a palindrome number",copy);
+printf("\n %d is not a palindrome number",num);
+
 return 0;
 }
 
